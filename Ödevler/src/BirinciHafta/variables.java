39fded371d9ea78607c4342c999408526a055eb1
Aslihan Evcil <aslihanevcil@outlook.com>
2022-09-20 16:48:55 +0300

    Birinci Hafta-2

---
+++ Ödevler/src/BirinciHafta/variables.java
@@ -0,0 +1,11 @@
+package BirinciHafta;
+
+public class variables {
+
+	public static void main(String[] args) {
+		int sayi1=10;
+		String mesaj ="Öğrenci Sayısı:";
+		System.out.println(mesaj +  sayi1);
+	}
+
+}
