21fa730890a8ff46399cde8fed127d8c2c716383
Aslihan Evcil <aslihanevcil@outlook.com>
2022-09-20 17:16:15 +0300

    Birinci Hafta-4

---
+++ Ödevler/src/BirinciHafta/conditionals.java
@@ -0,0 +1,16 @@
+package BirinciHafta;
+
+public class conditionals {
+
+	public static void main(String[] args) {
+		int sayi1=20;
+		if (sayi1<20) {
+			System.out.println("Sayı 20'den küçüktür.");
+		}else if (sayi1==20) {
+			System.out.println("Sayı 20'ye eşittir.");
+		}else {
+			System.out.println("Sayı 20'den büyüktür.");
+		}
+	}
+
+}
