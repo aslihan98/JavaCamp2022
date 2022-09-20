968e3214bd5a05ec6568e02215698e47f27b9945
Aslihan Evcil <aslihanevcil@outlook.com>
2022-09-20 17:11:54 +0300

    Birinci Hafta-3

---
+++ Ödevler/src/BirinciHafta/dataTypes.java
@@ -0,0 +1,16 @@
+package BirinciHafta;
+
+public class dataTypes {
+
+	public static void main(String[] args) {
+		int sayi1= 12;
+		double sayi2 =56.4;
+		char karakter='A';
+		String sehir="Ankara";
+		boolean dogruMu= true;
+		if (dogruMu==true) {
+			System.out.println("True");
+		}
+	}
+
+}
