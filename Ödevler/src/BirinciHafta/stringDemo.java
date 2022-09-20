8a71b3e70d84ad5caf163848fe36a8a17b813913
Aslihan Evcil <aslihanevcil@outlook.com>
2022-09-20 21:52:16 +0300

    Birinci Hafta-12

--- Ödevler/src/BirinciHafta/stringDemo.java
+++ Ödevler/src/BirinciHafta/stringDemo.java
@@ -5,7 +5,7 @@ public class stringDemo {
 	public static void main(String[] args) {
 		String mesaj= "Bugün hava çok güzel";
 		System.out.println(mesaj);
-		System.out.println("Eleman sayısı:"+mesaj.length());
+		/*System.out.println("Eleman sayısı:"+mesaj.length());
 		System.out.println("5.eleman"+ mesaj.charAt(4));
 		System.out.println(mesaj.concat("!"+"Yaşasın!"));
 		System.out.println(mesaj.startsWith("B"));
@@ -14,7 +14,20 @@ public class stringDemo {
 		mesaj.getChars(0, 4, karakterler, 0);
 		System.out.println(karakterler);
 		System.out.println(mesaj.indexOf('a'));
-		System.out.println(mesaj.lastIndexOf('e'));
+		System.out.println(mesaj.lastIndexOf('e'));*/
+	
+		String yeniMesaj =mesaj.replace(' ', '-');
+		System.out.println(yeniMesaj);
+		System.out.println(mesaj.substring(2, 5));
+		
+		for (String kelime:mesaj.split("")) {
+			System.out.println(kelime);
+		}
+		System.out.println(mesaj.toLowerCase());
+		System.out.println(mesaj.toUpperCase());
+	
 	}
 
+	
+	
 }
