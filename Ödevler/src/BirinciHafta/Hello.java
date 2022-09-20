62ddd670abe2cade701acae9a19c1d4d3a1db81a
Aslihan Evcil <aslihanevcil@outlook.com>
2022-09-20 16:43:15 +0300

    Birinci Hafta-1

---
+++ Ödevler/src/BirinciHafta/Hello.java
@@ -0,0 +1,12 @@
+package BirinciHafta;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		String message = "Hello World!";
+		System.out.println(message);
+	}
+
+}
