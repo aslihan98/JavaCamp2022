6d2fd3bbf00620d10db7140684c7700b9b039ce0
Aslihan Evcil <aslihanevcil@outlook.com>
2022-09-20 18:07:42 +0300

    Birinci Hafta- 6

---
+++ Ödevler/src/BirinciHafta/switchDemo.java
@@ -0,0 +1,29 @@
+package BirinciHafta;
+
+public class switchDemo {
+
+	public static void main(String[] args) {
+		char grade= 'A';	
+		
+		switch (grade) {
+		case 'A': 
+			System.out.println("Mükemmel! Geçtiniz!");
+		break;
+		case 'B': 
+			System.out.println("Çok güzel! Geçtiniz");
+		break;
+		case 'C': 
+			System.out.println(" Geçtiniz");
+		break;
+			default:
+				System.out.println("Geçersiz not!");
+		
+		}
+		
+		
+		
+		
+		
+	}
+
+}
