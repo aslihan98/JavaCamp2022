eb5eaae8febe2247d33866ffc446525827e97bbf
Aslihan Evcil <aslihanevcil@outlook.com>
2022-09-20 14:22:33 +0300

    Introduction

--- Dersler/src/BirinciHafta/intro.java
+++ Dersler/src/BirinciHafta/intro.java
@@ -2,4 +2,37 @@ package BirinciHafta;
 
 public class intro {
 
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+		
+		//değişken isimlendirme = camelCase
+		//
+		String message = "İlginizi çekebilir";
+		
+		System.out.println(message);
+		
+		int vade=12;
+		
+		double dolarDun =17.45;
+		double dolarBugun =18.50;
+		boolean dolarDustuMu= false;
+		
+		if (dolarDun<dolarBugun) {
+			System.out.println("Yukarı Oku");
+		}else if (dolarDun>dolarBugun) {
+			System.out.println("Aşağı Oku");
+		} else {
+			System.out.println("Eşittiroku");
+		}
+		
+		
+		String[] krediler= {"Hızlı Kredi", "Maaşını Halkbanktan Alan","Mutlu Emekli"};
+		System.out.println(krediler[1]);
+		for (int i = 0; i < krediler.length; i++) {
+			System.out.println(krediler[i]);
+		}
+	
+	
+	}
+
 }
